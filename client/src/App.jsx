89e83bb1886b26d7da9ced1f36bff8bd9b0621f1
seha3 <silviaherrera@gmail.com>
2019-05-22 12:18:19 -0500

    add pink background and FX button

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 // RHL only for front end development
 import { hot } from 'react-hot-loader';
+import VocalFX from './components/Effects.jsx';
+
 
 class App extends Component {
-  render() {
-    return <div>Welcome to a Modern Minimal React Boilerplate</div>;
+  render() { 
+    return <div>Welcome to a Modern Minimal React Boilerplate
+     
+      < VocalFX />
+    
+    </div>;
   }
 }
 // hot export works with RHL. Remove line 11 when starting fullstack integration
---
+++ client/src/components/Effects.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class VocalFX extends React.Component {
+//   contructor(props) {
+//     super(props);
+//     this.state = {
+//       done: false
+//     };
+//   }
+  render() {
+    return(
+      <button>whatever</button>
+    )
+  }
+  
+}
+export default VocalFX;
